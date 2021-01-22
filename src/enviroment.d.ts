6a33b1249b57837c70fc844a8328cb3bb6e90df8
ElCholoGamer <josedanielgrayson@gmail.com>
2021-01-21 20:38:37 -0500

    add process.env types

--- src/enviroment.d.ts
+++ src/enviroment.d.ts
@@ -1,5 +1,13 @@
 import CommandHandler from './structures/command/command-handler';
 
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			NODE_ENV: 'development' | 'production';
+		}
+	}
+}
+
 declare module 'discord.js' {
 	interface Client {
 		commandHandler: CommandHandler;
