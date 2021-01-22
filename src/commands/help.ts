24467d2e3c0c5dea761cb00d28c07906b2b6df93
ElCholoGamer <josedanielgrayson@gmail.com>
2021-01-21 20:58:41 -0500

    add help command

---
+++ src/commands/help.ts
@@ -0,0 +1,79 @@
+import { MessageEmbed, EmbedField } from 'discord.js';
+import Category from '../structures/category';
+import Command from '../structures/command/command';
+import CommandEvent from '../structures/command/command-event';
+import CommandHandler from '../structures/command/command-handler';
+import { prefix, embedColor } from '../config.json';
+import { formatPermissions } from '../utils';
+
+export default class Help extends Command {
+	public constructor(handler: CommandHandler) {
+		super('help', 'Shows help', Category.INFO, handler);
+	}
+
+	public async execute({ channel, client, args: [search] }: CommandEvent) {
+		const embed = new MessageEmbed().setColor(embedColor);
+
+		if (!search) {
+			embed
+				.setTitle("COOLSKELETON95'S TOTALLY COOL COMMANDS")
+				.setThumbnail(
+					client.user!.displayAvatarURL({ format: 'png', size: 256 })
+				);
+
+			const categories: Category[] = Object.values(Category).filter(
+				category => category.showHelp
+			);
+
+			const fields = categories.map<EmbedField>(category => {
+				const commands = this.commandHandler.commands.filter(
+					cmd => cmd.category === category
+				);
+
+				const list = commands.map(cmd => {
+					let inner = cmd.name;
+
+					const { aliases } = cmd.options;
+					if (aliases?.length) inner += ` (${aliases.join(', ')})`;
+
+					return `\`${inner}\``;
+				});
+
+				return {
+					name: category.displayName,
+					value: list.join(', ') || '[No commands]',
+					inline: false,
+				};
+			});
+
+			embed.addFields(fields);
+		} else {
+			const command = client.commandHandler.findCommand(search);
+			if (!command) return await channel.send("THAT COMMAND DOESN'T EXIST!");
+
+			const {
+				name,
+				description,
+				options: { aliases, permissions, usage = [] },
+			} = command;
+
+			embed
+				.setTitle(`COMMAND: \`${name}\``)
+				.setDescription(description)
+				.addField('USAGE', `\`${[prefix + name, ...usage].join(' ')}\``);
+
+			if (aliases?.length) {
+				embed.addField(
+					'ALIASES',
+					aliases.map(alias => `\`${alias}\``).join(', ')
+				);
+			}
+
+			if (permissions?.length) {
+				embed.addField('PERMISSIONS', formatPermissions(permissions));
+			}
+		}
+
+		await channel.send(embed);
+	}
+}
--- src/structures/category.ts
+++ src/structures/category.ts
@@ -1,10 +1,13 @@
 class Category {
-	private constructor(public readonly displayName: string) {}
+	private constructor(
+		public readonly displayName: string,
+		public readonly showHelp = true
+	) {}
 
 	public static INFO = new Category('Info');
 	public static FUN = new Category('Fun');
 	public static ECONOMY = new Category('Economy');
-	public static OWNER = new Category('Owner');
+	public static OWNER = new Category('Owner', false);
 }
 
 export default Category;
