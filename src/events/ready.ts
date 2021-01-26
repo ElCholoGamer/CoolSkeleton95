cdf914b2ee0a59ddc1614cc1e80d8fe898a3e2ca
ElCholoGamer <josedanielgrayson@gmail.com>
2021-01-26 13:26:02 -0500

    add bot activity

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,14 +1,20 @@
+import { ActivityOptions } from 'discord.js';
 import EventHandler from '../structures/event-handler';
+import { activity } from '../config.json';
 
 class ReadyHandler extends EventHandler('ready') {
 	public async execute() {
-		await Promise.all(
-			this.client.guilds.cache.map(guild =>
-				guild.me?.voice.connection?.disconnect()
-			)
-		);
+		const { user } = this.client;
+		if (!user) return;
+
+		await this.setActivity();
+		setInterval(() => this.setActivity(), 10 * 60 * 1000);
 
 		console.log(`Logged in as "${this.client.user?.tag}"!`);
+	}
+
+	private setActivity() {
+		return this.client.user?.setActivity(activity as ActivityOptions);
 	}
 }
 
